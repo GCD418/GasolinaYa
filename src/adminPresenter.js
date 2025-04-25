ccc535bae97321efc0ed712e07179360895a7990
gcd418 <patosquare418@gmail.com>
2025-04-25 16:25:20 -0400

    style: validación para litros negativos o stock actual superior a capacidad

--- src/adminPresenter.js
+++ src/adminPresenter.js
@@ -10,10 +10,32 @@ const form = document.getElementById("liters_form");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    if(!liter_quantity_input.value){
+        alert("Por favor ingrese la cantidad de litros");
+        return;
+    }
+    if(!liter_capacity_input.value){
+        alert("Por favor ingrese la cantidad de litros");
+        return;
+    }
+    
+    
     const liters = Number.parseInt(liter_quantity_input.value);
     const literCapacity = Number.parseInt(liter_capacity_input.value);
     const gasolinera = new Gasolinera(liters, literCapacity);
-
+    
+    if(liters < 0){
+        alert("La cantidad de litros no puede ser negativa");
+        return;
+    }
+    if(literCapacity <= 0){
+        alert("La capacidad del surtidor no puede ser negativa ni cero");
+        return;
+    }
+    if(liters > literCapacity){
+        alert("La cantidad de litros no puede ser mayor a la capacidad del surtidor");
+        return;
+    }
     result_div.innerHTML = `
     <p>La cantidad de litros de gasolina es: ${gasolinera.getFuelLiters()}</p>
     <p>La capacidad del surtidor es: ${gasolinera.getTotalCapacity()}</p>
