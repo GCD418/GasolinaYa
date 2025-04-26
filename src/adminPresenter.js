aac5598bc9ac9b65edd4a9cb7e4d07bb0a0e9e34
gcd418 <patosquare418@gmail.com>
2025-04-26 17:35:11 -0400

    style: apariencia de la vista del administrador mejorada

--- src/adminPresenter.js
+++ src/adminPresenter.js
@@ -9,6 +9,8 @@ const liter_capacity_input = document.getElementById("liter_capacity_input");
 const name_input = document.getElementById("name_input");
 const form = document.getElementById("liters_form");
 const form_container = document.getElementById("liters_form_container");
+let gasolinera = null;
+const gasolineras = new ModGasolineras();
 
 populateSelect();
 
@@ -31,7 +33,7 @@ form.addEventListener("submit", (event) => {
     const liters = Number.parseInt(liter_quantity_input.value);
     const literCapacity = Number.parseInt(liter_capacity_input.value);
     const name = name_input.value;
-    const gasolinera = new Gasolinera(liters, literCapacity, name);
+    gasolinera = new Gasolinera(liters, literCapacity, name);
     
     if(liters < 0){
         alert("La cantidad de litros no puede ser negativa");
@@ -45,15 +47,35 @@ form.addEventListener("submit", (event) => {
         alert("La cantidad de litros no puede ser mayor a la capacidad del surtidor");
         return;
     }
+    
+    showInformation();
+});
+
+select_gasolinera.addEventListener("change", (event) => {
+    const gasolineras = new ModGasolineras();
+    // const gasolinerasMap = gasolineras.getGasolineras();
+    // const selectedGasolinera = gasolinerasMap.get(event.target.value);
+    const selectedGasolinera = gasolineras.getGasolinera(event.target.value);
+    if(selectedGasolinera){
+        liter_quantity_input.value = selectedGasolinera.getFuelLiters();
+        liter_capacity_input.value = selectedGasolinera.getTotalCapacity();
+        name_input.value = selectedGasolinera.getName();
+        form_container.classList.remove("hidden");
+        gasolinera = selectedGasolinera;
+        showInformation();
+    }
+});
+
+function showInformation(){
     result_div.innerHTML = `
     <p>La cantidad de litros de gasolina es: ${gasolinera.getFuelLiters()}</p>
     <p>La capacidad del surtidor es: ${gasolinera.getTotalCapacity()}</p>
     <p>El porcentaje restante de combustible es: ${gasolinera.getFuelPercent()}%</p>
     `;
-
+    
     updateColorBasedOnPercentCapacity(gasolinera.getFuelPercent());
     name_fuel_station.innerHTML = `Estación de servicio "${gasolinera.getName()}"`;
-});
+}
 
 function updateColorBasedOnPercentCapacity(percent){
     if(percent <= 20){
@@ -65,7 +87,6 @@ function updateColorBasedOnPercentCapacity(percent){
 }
 
 function populateSelect(){
-    const gasolineras = new ModGasolineras();
     gasolineras.insertFakeData();
     const gasolinerasMap = gasolineras.getGasolineras();
     for (const [key, value] of gasolinerasMap) {
@@ -75,14 +96,3 @@ function populateSelect(){
         select_gasolinera.appendChild(option);
     }
 }
-select_gasolinera.addEventListener("change", (event) => {
-    const gasolineras = new ModGasolineras();
-    const gasolinerasMap = gasolineras.getGasolineras();
-    const selectedGasolinera = gasolinerasMap.get(event.target.value);
-    if(selectedGasolinera){
-        liter_quantity_input.value = selectedGasolinera.getFuelLiters();
-        liter_capacity_input.value = selectedGasolinera.getTotalCapacity();
-        name_input.value = selectedGasolinera.getName();
-        form_container.classList.remove("hidden");
-    }
-});
