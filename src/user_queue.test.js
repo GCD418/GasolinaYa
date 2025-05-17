fa897064f3632f11735328f89954edd3283ef601
christian-ledezma <chris.ledezma.s@gmail.com>
2025-05-17 02:19:21 -0400

    Feat: Funcion de calcular la posibilidad de cargar gasolina

--- src/user_queue.test.js
+++ src/user_queue.test.js
@@ -1,8 +1,11 @@
-import calculate_waiting_time from "./user_queue";
+import { calculate_waiting_time, estimate_fuel_load } from "./user_queue";
 
 describe("Agregar en la fila", () => {
   it("deberia calcular el tiempo de espera", () => {
     expect(calculate_waiting_time(4)).toEqual(20);
+  });
+  it("deberia estimar si tiene posibilidad de cargar combustible", () => {
+    expect(estimate_fuel_load(100, 2)).toEqual("Si");
   });
 });
 
