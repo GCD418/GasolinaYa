e6a1ca8f777eab62b03de1efea2a4dce4d03d7f9
Carla_Diana <carladianavgonzales@gmail.com>
2025-05-24 23:40:24 -0400

    feat: implementar getReservationMessage y lógica completa de reserva

--- src/ReserveTicket.js
+++ src/ReserveTicket.js
@@ -4,3 +4,11 @@ export function getAvailableStations(user, stations) {
     }
     return [];
   }
+
+export function getReservationMessage(user) {
+if (user.ticket) {
+    const { station, hour } = user.ticket;
+    return `Ya tienes un ticket reservado para la estación ${station} a las ${hour}.`;
+}
+return "No tienes ningún ticket reservado.";
+}
--- src/ReserveTicket.test.js
+++ src/ReserveTicket.test.js
@@ -1,13 +1,23 @@
-import { getAvailableStations } from "./ReserveTicket";
+import { getAvailableStations, getReservationMessage } from "./ReserveTicket";
 
 describe("Reserva de Tickets", () => {
-  it("Debe devolver todas las estaciones disponibles cuando el usuario no tiene ningún billete reservado", () => {
-    const user = { plate: "ABC123", ticket: null };
-    const stations = ["Cristo", "Asunción", "Manantial"];
+    it("Debe devolver todas las estaciones disponibles cuando el usuario no tiene ningún Ticket reservado", () => {
+        const user = { plate: "ABC123", ticket: null };
+        const stations = ["Cristo", "Asunción", "Manantial"];
+    
+        expect(getAvailableStations(user, stations)).toEqual(stations);
+      });
 
-    const result = getAvailableStations(user, stations);
+  it("Debe devolver un mensaje si el usuario ya tiene un ticket reservado.", () => {
+    const user = {
+      plate: "XYZ789",
+      ticket: { station: "Asunción", hour: "10:00" },
+    };
 
-    expect(result).toEqual(stations);
-
+    const msg = getReservationMessage(user);
+    expect(msg).toBe(
+      "Ya tienes un ticket reservado para la estación Asunción a las 10:00."
+    );
   });
+  
 });
