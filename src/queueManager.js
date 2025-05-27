35f8bb485b127e70cb190d0c576dca29eccac2f4
christian-ledezma <chris.ledezma.s@gmail.com>
2025-05-27 02:38:14 -0400

    Refactor: Extract funcion dispatchTableUpdateEvent

--- src/queueManager.js
+++ src/queueManager.js
@@ -76,15 +76,7 @@ async function confirmGasolineraSelection(popup, modUsuarios, modGasolineras) {
         await addUsuarioToGasolineraQueue(selectedPlaca, selectedGasolinera, modUsuarios, modGasolineras);
         
         closePopup(popup);
-        
-        const updateEvent = new CustomEvent('tableUpdateRequired', {
-            detail: { 
-                type: 'queueUpdate',
-                placa: selectedPlaca,
-                gasolinera: selectedGasolinera
-            }
-        });
-        document.dispatchEvent(updateEvent);
+        dispatchTableUpdateEvent(selectedPlaca, selectedGasolinera);
 
         const container = document.querySelector("#service_stations_container");
         if (container) {
@@ -107,6 +99,16 @@ function getSelectedValues(popup) {
     return { selectedPlaca, selectedGasolinera };
 }
 
+function dispatchTableUpdateEvent(selectedPlaca, selectedGasolinera) {
+    const updateEvent = new CustomEvent('tableUpdateRequired', {
+        detail: {
+            type: 'queueUpdate',
+            placa: selectedPlaca,
+            gasolinera: selectedGasolinera
+        }
+    });
+    document.dispatchEvent(updateEvent);
+}
 
 async function addUsuarioToGasolineraQueue(placa, gasolineraName, modUsuarios, modGasolineras) {
     const usuario = modUsuarios.getUsuario(placa);
