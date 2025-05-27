be86727794cd24fb66f1c71bf7293a1b44616332
gcd418 <patosquare418@gmail.com>
2025-05-26 23:23:31 -0400

    test: prueba de obtención de cantidad de Mangueras disponibles

--- cypress/e2e/adminPresenter.cy.js
+++ cypress/e2e/adminPresenter.cy.js
@@ -5,4 +5,11 @@ describe("adminPresenter", () => {
     cy.get("#select_gasolinera").select("GasolineraDePrueba");
     cy.get("h1").should("contain", "Estación de servicio \"GasolineraDePrueba\"");
   });
+
+  it("Debería mantener el número de mangueras actualizado", () => {
+    cy.visit("/adminIndex.html");
+    cy.wait(2000);
+    cy.get("#select_gasolinera").select("El Cristo");
+    cy.get("#hoses_input").should("have.value", "5");
+  });
 });
